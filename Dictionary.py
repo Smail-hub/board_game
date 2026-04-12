a0e67cbf845d2e25c8315350eba724356bd7584b
Smail-hub <amilsafin@gmail.com>
2026-04-12 22:49:34 +0500

    Проект готов!

--- Dictionary.py
+++
@@ -1,7 +0,0 @@
-users_list = [
-    ["+111123455", "Tom"],
-    ["+384767557", "Bob"],
-    ["+958758767", "Alice"]
-]
-users_dict = dict(users_list)
-print(users_dict)
--- main.py
+++ main.py
@@ -5,60 +5,119 @@ from faker import Faker
 
 fake = Faker("ru_RU")
 
-skills = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар",
-          "Стремительный удар", "Кислотный взгляд", "Тайный побег",
-          "Ледяной выстрел", "Огненный заряд"]
+skills = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд",
+]
 
-letters_mapping = {'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
-                  'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
-                  'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
-                  'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
-                  'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
-                  'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
-                  'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
-                  'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
-                  'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
-                  'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
-                  'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
-                  'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
-                  'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
-                  'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
-                  'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
-                  'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
-                  'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
-                  'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
-                  'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
-                  'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
-                  'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
-                  'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
-                  ' ': ' '}
+letters_mapping = {
+    "а": "а͠",
+    "б": "б̋",
+    "в": "в͒͠",
+    "г": "г͒͠",
+    "д": "д̋",
+    "е": "е͠",
+    "ё": "ё͒͠",
+    "ж": "ж͒",
+    "з": "з̋̋͠",
+    "и": "и",
+    "й": "й͒͠",
+    "к": "к̋̋",
+    "л": "л̋͠",
+    "м": "м͒͠",
+    "н": "н͒",
+    "о": "о̋",
+    "п": "п̋͠",
+    "р": "р̋͠",
+    "с": "с͒",
+    "т": "т͒",
+    "у": "у͒͠",
+    "ф": "ф̋̋͠",
+    "х": "х͒͠",
+    "ц": "ц̋",
+    "ч": "ч̋͠",
+    "ш": "ш͒͠",
+    "щ": "щ̋",
+    "ъ": "ъ̋͠",
+    "ы": "ы̋͠",
+    "ь": "ь̋",
+    "э": "э͒͠͠",
+    "ю": "ю̋͠",
+    "я": "я̋",
+    "А": "А͠",
+    "Б": "Б̋",
+    "В": "В͒͠",
+    "Г": "Г͒͠",
+    "Д": "Д̋",
+    "Е": "Е",
+    "Ё": "Ё͒͠",
+    "Ж": "Ж͒",
+    "З": "З̋̋͠",
+    "И": "И",
+    "Й": "Й͒͠",
+    "К": "К̋̋",
+    "Л": "Л̋͠",
+    "М": "М͒͠",
+    "Н": "Н͒",
+    "О": "О̋",
+    "П": "П̋͠",
+    "Р": "Р̋͠",
+    "С": "С͒",
+    "Т": "Т͒",
+    "У": "У͒͠",
+    "Ф": "Ф̋̋͠",
+    "Х": "Х͒͠",
+    "Ц": "Ц̋",
+    "Ч": "Ч̋͠",
+    "Ш": "Ш͒͠",
+    "Щ": "Щ̋",
+    "Ъ": "Ъ̋͠",
+    "Ы": "Ы̋͠",
+    "Ь": "Ь̋",
+    "Э": "Э͒͠͠",
+    "Ю": "Ю̋͠",
+    "Я": "Я̋",
+    " ": " ",
+}
 
-os.makedirs("results", exist_ok=True)
 
-for number in range(1, 11):
-    skills_3 = random.sample(skills, 3)
+def go_to_work():
+    os.makedirs("results", exist_ok=True)
 
-    rune_skills = []
-    for skill in skills_3:
-        result = skill
-        for letter, rune in letters_mapping.items():
-            result = result.replace(letter, rune)
-        rune_skills.append(result)
+    for number in range(1, 11):
+        skills_3 = random.sample(skills, 3)
 
-    context = {
-        "first_name": fake.first_name_female(),
-        "last_name": fake.last_name_female(),
-        "town": fake.city(),
-        "job": fake.job(),
-        "strength": random.randint(3, 18),
-        "agility": random.randint(3, 18),
-        "endurance": random.randint(3, 18),
-        "intelligence": random.randint(3, 18),
-        "luck": random.randint(3, 18),
-        "skill_1": rune_skills[0],
-        "skill_2": rune_skills[1],
-        "skill_3": rune_skills[2],
-    }
+        rune_skills = []
+        for skill in skills_3:
+            result = skill
+            for letter, rune in letters_mapping.items():
+                result = result.replace(letter, rune)
+            rune_skills.append(result)
 
-    output_file = "results/result_{}.svg".format(number)
-    file_operations.render_template("charsheet.svg", output_file, context)
+        context = {
+            "first_name": fake.first_name_female(),
+            "last_name": fake.last_name_female(),
+            "town": fake.city(),
+            "job": fake.job(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": rune_skills[0],
+            "skill_2": rune_skills[1],
+            "skill_3": rune_skills[2],
+        }
+
+        output_file = "results/result_{}.svg".format(number)
+        file_operations.render_template("charsheet.svg", output_file, context)
+
+
+if __name__ == "__main__":
+    go_to_work()
